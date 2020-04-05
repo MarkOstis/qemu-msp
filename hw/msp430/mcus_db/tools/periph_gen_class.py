e96ed8a6b50db8e449be98c6dac0cd946f33c0c5
MarkOstis <mark.ostis@gmail.com>
2020-04-05 21:42:02 +0300

    MSP430 Peripheral generator were improved.

--- hw/msp430/mcus_db/tools/periph_gen_class.py
+++ hw/msp430/mcus_db/tools/periph_gen_class.py
@@ -32,8 +32,119 @@ class periph_gen:
     def func_get_reg_bits_mask_name(self, reg):
         return "REG_" + reg['name'].upper() + "_BITS_MASK"
 
-    def func_get_field_bit_mask_name(self, field):
-        return "REG_" + field['name'].upper() + "_BITS_MASK"
+    def func_get_field_bit_mask_name(self, reg, field):
+        return "REG_" + reg['name'].upper() + "_" + field['name'].upper() + "_BITS_MASK"
+
+    def is_reg_equal(self, reg1, reg2):
+        is_equal = 0
+
+        reg1_offset = int(reg1['addressOffset'], 16)
+        reg1_size = int(reg1['size'])//8
+
+        reg2_offset = int(reg2['addressOffset'], 16)
+        reg2_size = int(reg2['size'])//8
+
+        if ((reg1_offset == reg2_offset) and (reg1_size == reg2_size)):
+            is_equal = 1
+        return is_equal
+
+    def is_reg_duplicated(self, regs, reg):
+        is_dup = 0
+
+        for r in regs:
+            if (r['name'] != reg['name']):
+                if (self.is_reg_equal(reg, r)):
+                    is_dup = 1
+        return is_dup
+
+    def get_reg_fields(self, reg):
+        fields = []
+        try:
+            f = reg['fields']['field']
+            try:
+                f1 = f[0]
+                fields = f
+            except:
+                fields.append(f)
+        except:
+            pass
+        return fields
+
+    def is_child_of_specific_reg(self, parent_reg, child_reg):
+        is_child = 0
+
+        p_reg_offset = int(parent_reg['addressOffset'], 16)
+        p_reg_size = int(parent_reg['size'])//8
+
+        c_reg_offset = int(child_reg['addressOffset'], 16)
+        c_reg_size = int(child_reg['size'])//8
+
+        if ((c_reg_offset >= p_reg_offset) and (c_reg_offset < p_reg_offset + p_reg_size) and (c_reg_size < p_reg_size)):
+            is_child = 1
+        return is_child
+
+    def is_child_reg(self, regs, reg):
+        is_child = 0
+        for r in regs:
+            if (r['name'] != reg['name']):
+                if (self.is_child_of_specific_reg(r, reg) == 1):
+                    is_child = 1
+        return is_child
+
+    def is_first_of_duplicated(self, regs, reg):
+        is_first = 0
+        for r in regs:
+            if (r['name'] == reg['name']):
+                is_first = 1
+                break
+            elif (self.is_reg_equal(r, reg) == 1):
+                break
+        return is_first
+
+    def is_duplicated_regs_has_fields(self, regs, reg):
+        is_has = 0
+        dup_regs = []
+        dup_regs.append(reg)
+        for r in regs:
+            if (r['name'] != reg['name']):
+                if (self.is_reg_equal(reg, r)):
+                    dup_regs.append(r)
+        for r in dup_regs:
+            fields = self.get_reg_fields(r)
+            if (len(fields) != 0):
+                is_has = 1
+                break
+        return is_has
+
+    def find_all_childs(self, regs, parent_reg):
+        childs = []
+        for r in regs:
+            if (self.is_child_of_specific_reg(parent_reg, r) == 1):
+                childs.append(r)
+        return childs
+
+    def get_parrent_reg(self, regs, child_reg):
+        p_reg = None
+        for r in regs:
+            if (r['name'] != child_reg['name']):
+                if (self.is_child_of_specific_reg(r, child_reg) == 1):
+                    p_reg = r
+        return p_reg
+
+    def check_reg(self, regs, reg):
+        valid = 0
+        if (self.is_child_reg(regs, reg) == 0):
+            if (self.is_reg_duplicated(regs, reg) == 0):
+                valid = 1
+            else:
+                fields = self.get_reg_fields(reg)
+                if (len(fields) > 0):
+                    valid = 1
+                elif (self.is_duplicated_regs_has_fields(regs, reg) == 0):
+                    if (self.is_first_of_duplicated(regs, reg) == 1):
+                        valid = 1
+
+        return valid
 
 
     def number_to_bit_mask(self, number):
@@ -50,22 +161,26 @@ class periph_gen:
     def add_define_regs_reset_value(self, regs):
         buff = "// REGISTERS RESET VALUE\n"
         for r in regs:
-            buff += self.add_define_value("REG_" + r['name'].upper() + "_RESET_VALUE", r['resetValue'])
+            if (self.check_reg(regs, r) == 1):
+                buff += self.add_define_value("REG_" + r['name'].upper() + "_RESET_VALUE", r['resetValue'])
         return buff
 
     def add_define_reg_bit_field(self, regs):
         buff = ""
         for r in regs:
             buff += "// FIELDS FOR REGISTER : " + r['name'].upper() + "\n"
-            field = None
-            try:
-                fields = r['fields']['field']
+            fields = self.get_reg_fields(r)
+            if (len(fields) > 0):
+                p_reg = self.get_parrent_reg(regs, r)
+                bit_offset = 0
+                if (p_reg != None):
+                    bit_offset = (int(r['addressOffset'], 16) - int(p_reg['addressOffset'], 16)) * 8
                 for f in fields:
                     bit_width = f['bitWidth']
                     bit_mask = str(self.number_to_bit_mask(int(bit_width)))
-                    buff += self.add_define_value(self.func_get_field_bit_mask_name(f), "(" + bit_mask + " << " + f['bitOffset'] +")")
-            except:
-                fields = None
+                    f_bit_offset = bit_offset + int(f['bitOffset'])
+
+                    buff += self.add_define_value(self.func_get_field_bit_mask_name(r, f), "(" + bit_mask + " << " + str(f_bit_offset) +")")
 
             buff += "\n"
         return buff
@@ -73,18 +188,19 @@ class periph_gen:
     def add_define_reg_bits_mask(self, regs):
         buff = "// REGISTERS BITS MASKS\n"
         for r in regs:
-            bits_mask = 0
-            off_set = 0
-            try:
-                fields = r['fields']['field']
-                for f in fields:
-                    bit_width = f['bitWidth']
-                    bits_mask |= (self.number_to_bit_mask(int(bit_width)) << int(f['bitOffset']))
-            except:
-                reg_size = int(r['size'])
-                for bit in range(reg_size):
-                    bits_mask |= (1 << bit)
-            buff += self.add_define_value(self.func_get_reg_bits_mask_name(r), str(hex(bits_mask)))
+            if (self.check_reg(regs, r) == 1):
+                bits_mask = 0
+                off_set = 0
+                fields = self.get_reg_fields(r)
+                if (fields != []):
+                    for f in fields:
+                        bit_width = f['bitWidth']
+                        bits_mask |= (self.number_to_bit_mask(int(bit_width)) << int(f['bitOffset']))
+                else:
+                    reg_size = int(r['size'])
+                    for bit in range(reg_size):
+                        bits_mask |= (1 << bit)
+                buff += self.add_define_value(self.func_get_reg_bits_mask_name(r), str(hex(bits_mask)))
         buff += "\n"
         return buff
 
@@ -95,7 +211,8 @@ class periph_gen:
                 "   MemoryRegion region;\n\n"
 
         for r in regs:
-            buff += "   uint32_t " + r['name'].lower() + ";\n"
+            if (self.check_reg(regs, r) == 1):
+                buff += "   uint32_t " + r['name'].lower() + ";\n"
 
         buff += "} " + self.DEV_STATE_NAME + ";\n\n"
         return buff
@@ -105,24 +222,37 @@ class periph_gen:
 
     # FUNCTION DEFINITION
 
-    def add_func_def_reg_read(self, reg):
-        buff =  "static uint64_t " + self.func_get_reg_read_name(reg) + "(" + self.DEV_STATE_NAME + " *dev, uint32_t *reg) {\n" \
-                "    return ((*reg) & " + self.func_get_reg_bits_mask_name(reg) +");\n" \
+    def add_func_def_reg_read(self, regs, reg):
+        buff =  "static uint64_t " + self.func_get_reg_read_name(reg) + "(" + self.DEV_STATE_NAME + " *dev, uint32_t *reg, uint32_t offset) {\n" \
+                "    uint32_t value = ((*reg) & " + self.func_get_reg_bits_mask_name(reg) + ") >> (offset * 8);\n" \
+                "    return value;\n" \
                 "}\n\n"
         return buff
 
-    def add_func_def_reg_write(self, reg):
-        buff =  "static void " + self.func_get_reg_write_name(reg) + "(" + self.DEV_STATE_NAME + " *dev, uint32_t *reg, uint64_t val, unsigned int size) {\n" \
-                "    uint32_t new_value = val & " + self.func_get_reg_bits_mask_name(reg) + ";\n" \
+    def add_func_def_reg_write(self, regs, reg):
+        buff =  "static void " + self.func_get_reg_write_name(reg) + "(" + self.DEV_STATE_NAME + " *dev, uint32_t *reg, uint32_t offset, uint64_t val, unsigned int size) {\n" \
+                "    uint32_t new_value = (val << (offset * 8))  & " + self.func_get_reg_bits_mask_name(reg) + ";\n" \
                 "    if ((*reg) != new_value) {\n"
-        
-        try:
-            fields = reg['fields']['field']
+
+        if (reg['name'] == "RTCADOWDAY"):
+            print("RTCADOWDAY")
+
+        fields = self.get_reg_fields(reg)
+        if (len(fields) > 0):
             for f in fields:
-                buff += "        if ((new_value & " + self.func_get_field_bit_mask_name(f) + ") != ((*reg) & " + self.func_get_field_bit_mask_name(f) + ")) {\n"\
-                        "        }\n"
-        except:
-            pass
+                buff += "        if ((new_value & " + self.func_get_field_bit_mask_name(reg, f) + ") != ((*reg) & " + self.func_get_field_bit_mask_name(reg, f) + ")) {\n"\
+                        "        }\n\n"
+        else:
+            # Check if there are child regs:
+            childs_regs = self.find_all_childs(regs, reg)
+            if (len(childs_regs) > 0):
+                for ch_r in childs_regs:
+                    fields = self.get_reg_fields(ch_r)
+                    if (len(fields) > 0):
+                        for f in fields:
+                            buff += "        if ((new_value & " + self.func_get_field_bit_mask_name(ch_r, f) + ") != ((*reg) & " + self.func_get_field_bit_mask_name(ch_r, f) + ")) {\n"\
+                                    "        }\n\n"
+
         buff += "        (*reg) = new_value;\n"
         buff += "    }\n" \
                 "}\n\n"
@@ -131,46 +261,57 @@ class periph_gen:
     def add_func_def_reg_read_write(self, regs):
         buff = ""
         for r in regs:
-            buff += "// REG: " + r['name'].upper() + "\n"
-            buff += self.add_func_def_reg_read(r) + self.add_func_def_reg_write(r)
+            if (self.check_reg(regs, r) == 1):
+                buff += "// REG: " + r['name'].upper() + "\n"
+                buff += self.add_func_def_reg_read(regs, r) + self.add_func_def_reg_write(regs, r)
         return buff
 
 
     def add_func_def_periph_regs_read(self, regs):
+
         buff = FUNC_PERIPH_REGS_READ + " {\n" \
                 "    " + self.DEV_STATE_NAME + " *s = " + self.MCU_PERIPH + "(opaque);\n" \
-                "    uint64_t retvalue = 0;\n\n" \
-                "    switch (addr) {\n"
-
+                "    uint64_t retvalue = 0;\n\n"
+        is_first = 0
         for r in regs:
-            buff += "        case " + r['name'] + ":\t\t\t{ retvalue = " + self.func_get_reg_read_name(r) + "(s, &s->" + r['name'].lower() + ");\t\t} break;\n"
+            if (self.check_reg(regs, r) == 1):
+                if (is_first == 0):
+                    is_first = 1
+                    buff += "    if "
+                else:
+                    buff += " else if "
+                buff += "((addr >= " + r['name'] + ") && ((addr + size) <= ("  + r['name'] + " + " + str(int(r['size'])//8) + "))) {\n" \
+                        "        retvalue = " + self.func_get_reg_read_name(r) + "(s, &s->" + r['name'].lower() + ", (addr - " + r['name'] + "));\n" \
+                        "    }"
 
-        buff += "        default: {\n" \
-                "            qemu_log_mask(LOG_GUEST_ERROR, \"%s: Bad offset 0x%\"HWADDR_PRIx\"\\n\", __func__, addr);" \
-                "        }\n" \
-                "        break;\n" \
+        buff += " else {\n" \
+                "        qemu_log_mask(LOG_GUEST_ERROR, \"%s: Bad offset 0x%\"HWADDR_PRIx\", size = %u\\n\", __func__, addr, size);\n" \
                 "    }\n"
         # end of function
         buff += "    return retvalue;\n}\n\n"
-
         return buff
 
     def add_func_def_periph_regs_write(self, regs):
         buff = FUNC_PERIPH_REGS_WRITE + " {\n" \
-                "    " + self.DEV_STATE_NAME + " *s = " + self.MCU_PERIPH + "(opaque);\n\n" \
-                "    switch (addr) {\n"
+                "    " + self.DEV_STATE_NAME + " *s = " + self.MCU_PERIPH + "(opaque);\n\n"
 
+        is_first = 0
         for r in regs:
-            buff += "        case " + r['name'].upper() + ":\t\t\t{ " + self.func_get_reg_write_name(r) + "(s, &s->" + r['name'].lower() + ", value, size);\t\t} break;\n"
+            if (self.check_reg(regs, r) == 1):
+                if (is_first == 0):
+                    is_first = 1
+                    buff += "    if "
+                else:
+                    buff += " else if "
+                buff += "((addr >= " + r['name'] + ") && ((addr + size) <= ("  + r['name'] + " + " + str(int(r['size'])//8) + "))) {\n" \
+                        "        " + self.func_get_reg_write_name(r) + "(s, &s->" + r['name'].lower() + ", (addr - " + r['name'] + "), value, size);\n" \
+                        "    }"
 
-        buff += "        default: {\n" \
-                "            qemu_log_mask(LOG_GUEST_ERROR, \"%s: Bad offset 0x%\"HWADDR_PRIx\"\\n\", __func__, addr);" \
-                "        }\n" \
-                "        break;\n" \
+        buff += " else {\n" \
+                "        qemu_log_mask(LOG_GUEST_ERROR, \"%s: Bad offset 0x%\"HWADDR_PRIx\", size = %u\\n\", __func__, addr, size);\n" \
                 "    }\n"
         # end of function
         buff += "\n}\n\n"
-
         return buff
 
     def add_func_def_reset_callback(self, regs):
@@ -178,7 +319,8 @@ class periph_gen:
                 "    " + self.DEV_STATE_NAME + " *device = " + self.MCU_PERIPH + "(dev);\n\n"
 
         for r in regs:
-            buff += "   device->" + r['name'].lower() + " = " + r['resetValue'] + ";\n"
+            if (self.check_reg(regs, r) == 1):
+                buff += "   device->" + r['name'].lower() + " = " + r['resetValue'] + ";\n"
 
         # end of function
         buff += "\n}\n\n"
@@ -196,7 +338,7 @@ class periph_gen:
     def add_func_def_init_callback(self):
         buff = FUNC_PERIPH_DEVICE_INIT + " {\n" \
                 "    " + self.DEV_STATE_NAME + " *s = " + self.MCU_PERIPH + "(obj);\n\n" \
-                "    memory_region_init_io(&s->region, obj, &mmio_ops, s, " + self.TYPE_MCU_PERIPH + ", PERIPHERAL_OFFSET);\n" \
+                "    memory_region_init_io(&s->region, obj, &mmio_ops, s, " + self.TYPE_MCU_PERIPH + ", SIZE);\n" \
                 "    sysbus_init_mmio(SYS_BUS_DEVICE(obj), &s->region);\n" \
                 "}\n\n"
         return buff
--- hw/msp430/mcus_db/tools/periph_generator.py
+++ hw/msp430/mcus_db/tools/periph_generator.py
@@ -5,32 +5,28 @@ import argparse
 import os
 from periph_gen_class import periph_gen
 
-# MAIN
-parser = argparse.ArgumentParser(description='Process input arguments.')
-
 MCU_NAME="MSP430FR5739"
 
-with open('/home/marktwis/workspace/QEMU/qemu-msp/hw/msp430/mcus_db/json/MSP430FR5739.json') as json_file:
-    all_json = json.load(json_file)
 
-peripheral_json = all_json['device']['peripherals']['peripheral']
-
-
-# Create output folder
-out_dir_name="./../peripheral/" + MCU_NAME
-if not os.path.exists(out_dir_name):
-    os.makedirs(out_dir_name)
-
-for p in peripheral_json:
-    if ((p['name'] == "Port_A") or (p['name'] == "Port_B") or (p['name'] == "Port_J")):
+def create_peripheral_files(out_dir_name, mcu_name, peripheral_json):
+    for p in peripheral_json:
+    #    if ((p['name'] == "Port_A") or (p['name'] == "Port_B") or (p['name'] == "Port_J")):
         include_data = ""
         defines_data = ""
         types_data = ""
         decl_data = ""
         def_data = ""
 
+        regs = None
+        regs_from_p = p['registers']['register']
 
-        regs = p['registers']['register']
+        try:
+            r = regs_from_p[0]
+            regs = regs_from_p
+        except:
+            regs = []
+            regs.append(regs_from_p)
+
         reg_offset_addr=regs[0]['addressOffset']
 
 
@@ -39,7 +35,12 @@ for p in peripheral_json:
         _MCU_PERIPH = MCU_NAME.upper() + "_" + p['name'].upper()   
         _MCU_PERIPH_STR = "\"" + MCU_NAME.lower() + "_" + p['name'].lower() + "\""
         _PERIPH_OFFSET = reg_offset_addr
-        p_gen = periph_gen(_DEV_STATE_NAME, _TYPE_MCU_PERIPH, _MCU_PERIPH, _MCU_PERIPH_STR, _PERIPH_OFFSET)
+        _PERIPHERAL_SIZE = str(get_peripheral_memory_size(p))
+        p_gen = periph_gen(_DEV_STATE_NAME,
+                           _TYPE_MCU_PERIPH,
+                           _MCU_PERIPH,
+                           _MCU_PERIPH_STR,
+                           _PERIPH_OFFSET)
 
 
 
@@ -51,13 +52,16 @@ for p in peripheral_json:
         # Add registers offsets
 
         defines_data = p_gen.add_define_value(_TYPE_MCU_PERIPH, _MCU_PERIPH_STR)
-        defines_data += p_gen.add_define_value("SIZE", len(regs))
+        defines_data += p_gen.add_define_value("SIZE", _PERIPHERAL_SIZE)
         defines_data += p_gen.add_define_value(_MCU_PERIPH + "(obj)", "OBJECT_CHECK(" + _DEV_STATE_NAME + ", (obj), " + _TYPE_MCU_PERIPH + ")")
 
         defines_data += "// REGISTERS:\n" + p_gen.add_define_value("PERIPHERAL_OFFSET", reg_offset_addr) + "\n"
         for r in regs:
-            reg_offset = int(r['addressOffset'], 16) - int(reg_offset_addr, 16)
-            defines_data += p_gen.add_define_value(r['name'], reg_offset)
+            if (r['name'] == "UCA0IE"):
+                print("UCA0IE")
+            if (p_gen.check_reg(regs, r) == 1):
+                reg_offset = int(r['addressOffset'], 16) - int(reg_offset_addr, 16)
+                defines_data += p_gen.add_define_value(r['name'], reg_offset)
 
         defines_data += "\n"
         defines_data += p_gen.add_define_regs_reset_value(regs) + "\n"
@@ -70,11 +74,6 @@ for p in peripheral_json:
 
         # Add functions
         # decl_data += "\n\n\n // Local functions declarations\n"
-        # decl_data += p_gen.add_func_decl_reg_read_write(regs)
-        # decl_data += p_gen.add_func_decl_periph_regs_read()
-        # decl_data += p_gen.add_func_decl_periph_regs_write()
-        # decl_data += p_gen.add_func_decl_reset_callback()
-        # decl_data += p_gen.add_func_decl_init_callback()
 
         def_data += "\n\n\n // Local functions definitions\n"
         def_data += p_gen.add_func_def_reg_read_write(regs)
@@ -90,11 +89,97 @@ for p in peripheral_json:
         def_data += p_gen.add_func_def_register_types()
         def_data += p_gen.add_type_init()
         
-
-
-
-
         file_data = include_data + defines_data + types_data + decl_data + def_data
-        out_file_name=out_dir_name + "/" + MCU_NAME + "_" + p['name'] + ".c"
+        out_file_name=out_dir_name + "/" + mcu_name + "_" + p['name'] + ".c"
         with open(out_file_name, 'w') as out_file:
             out_file.write(file_data)
+
+def get_peripheral_regs(p):
+    regs = None
+    regs_from_p = p['registers']['register']
+
+    try:
+        r = regs_from_p[0]
+        regs = regs_from_p
+    except:
+        regs = []
+        regs.append(regs_from_p)
+
+    return regs
+
+def get_peripheral_memory_offset(p):
+    regs = get_peripheral_regs(p)
+    
+    if (len(regs) > 0):
+        return regs[0]['addressOffset']
+    else:
+        print("Error: peripheral does not have registers")
+        exit(-1)
+    return 0
+
+def get_reg_with_biggest_offset(regs):
+    reg = None
+    offset = 0
+    for r in regs:
+        r_offset = int(r['addressOffset'], 16)
+        if (r_offset > offset):
+            offset = r_offset
+            reg = r
+    return reg
+
+
+def get_peripheral_memory_size(p):
+    regs = get_peripheral_regs(p)
+    if (len(regs) > 0):
+        reg = get_reg_with_biggest_offset(regs)
+        base_offset = int(get_peripheral_memory_offset(p), 16)
+        p_size = (int(reg['addressOffset'], 16) - base_offset) + int(reg['size'])//8
+    return p_size
+
+
+def create_mcu_periph_file(out_dir_name, mcu_name, peripheral_json):
+    def_data =  "typedef struct {\n" \
+                "    const char *device_name;\n" \
+                "    const char *device_name_to_create;\n" \
+                "    uint32_t device_base_addr;\n" \
+                "}" + mcu_name.lower() + "_peripheral_t;\n\n"
+
+    def_data += "static " + mcu_name.lower() +  "_peripheral_t periphs[] = {\n"
+    for p in peripheral_json:
+        p_device_name = "\"" + MCU_NAME.lower() + "_" + p['name'].lower() + "\""
+        p_offset = get_peripheral_memory_offset(p)
+
+        def_data +=                 "    {" + p_device_name + ", " + p_device_name + ", " + p_offset + "},\n"
+
+    def_data +=                     "};\n"
+
+    out_file_name=out_dir_name + "/" + mcu_name + ".c"
+    with open(out_file_name, 'w') as out_file:
+        out_file.write(def_data)
+
+def create_mcu_make_file_data(out_dir_name, mcu_name, peripheral_json):
+    data = ""
+    for p in peripheral_json:
+        data += MCU_NAME.upper() + "_" + p['name'] + ".o\n"
+
+    out_file_name=out_dir_name + "/" + mcu_name + "_MakeFile.txt"
+    with open(out_file_name, 'w') as out_file:
+        out_file.write(data)
+
+
+# MAIN
+parser = argparse.ArgumentParser(description='Process input arguments.')
+
+with open('/home/marktwis/workspace/QEMU/qemu-msp/hw/msp430/mcus_db/json/MSP430FR5739.json') as json_file:
+    all_json = json.load(json_file)
+
+# Create output folder
+out_dir_name="./../peripheral/" + MCU_NAME
+if not os.path.exists(out_dir_name):
+    os.makedirs(out_dir_name)
+
+peripheral_json = all_json['device']['peripherals']['peripheral']
+
+create_peripheral_files(out_dir_name, MCU_NAME, peripheral_json)
+create_mcu_periph_file(out_dir_name, MCU_NAME, peripheral_json)
+create_mcu_make_file_data(out_dir_name, MCU_NAME, peripheral_json)
